f7eaf6b6d4776be24b34dd4e74cd8026da0c648c
Koushik Dasika <koushikdasika@gmail.com>
2016-05-31 18:47:15 -0400

    solved the heartbeat problem

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 var ws = new SockJS('http://127.0.0.1:15674/stomp');
 var client = Stomp.over(ws);
 
+client.heartbeat.outgoing = 20000; // client will send heartbeats every 20000ms
+client.heartbeat.incoming = 0;     // client does not want to receive heartbeats
+
 var on_connect = function() {
   console.log('connected');
   var subscription = client.subscribe("server", function(message) {
@@ -9,9 +12,6 @@ var on_connect = function() {
     $("#responses").prepend("<li>" + blah.time + "</li>")
 
   });
-  client.onheartbeat = function() {
-    console.log('heartbeat');
-  };
 };
 var on_error =  function() {
    console.log('error');
