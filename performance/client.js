35ded967f0f4ed8038be88414dc2df8105e0dcb4
Koushik Dasika <koushikdasika@gmail.com>
2016-06-13 17:34:19 -0400

    hotel stays displaying fine

--- performance/client.js
+++ performance/client.js
@@ -10,12 +10,17 @@ var hotelChannel = "hotel_stays"
 var airfareChannel = "airfares"
 var iterations = 30;
 var hotelCode = "0013522";
+var lengthOfStay = 3;
+var lengthOfResponse = 50;
 
 var on_connect = function() {
   console.log('connected');
   var subscription = client.subscribe(hotelChannel, function(message) {
     var response = JSON.parse(message.body)
     console.log("message from server", response)
+    $("."+ response.check_in_at).append(
+      "<td>"+ message.body.substring(0, lengthOfResponse) + "</td>"
+    );
   });
 
   var subscription = client.subscribe(airfareChannel, function(message) {
@@ -29,37 +34,53 @@ var on_error =  function() {
 
 client.connect('guest', 'guest', on_connect, on_error, '/');
 
-$(document).ready(function() {
-  initializeHotelDates();
-})
-
-var initializeHotelDates = function() {
-  var startDate = new moment();
-  for(var i = 0; i < iterations; i++) {
-    populateHotelTable(i, startDate);
-  };
-}
-var populateHotelTable = function(counter, date) {
-  $(".hotel-stays-table").append(
-    "<tr><td>" + counter + "</td>" +
-    "<td>" + hotelCode + "</td>" +
-    "<td>" + moment(date).add('days', counter).format('YYYY-MM-DD') + "</td>" +
-    "<td>" + moment(date).add('days', counter + 1).format('YYYY-MM-DD') + "</td>"
-  )
+var hotelRequestKey = function(counter, date) {
+  return hotelCode + "|" +
+    moment(date).add(counter, 'days').format("YYYYMMDD") + "|" +
+    moment(date).add(counter + lengthOfStay, 'days').format("YYYYMMDD") + "|2|1";
 }
 
-var sendHotelStayRequest = function (depart_date, return_date) {
+
+var sendHotelStayRequest = function (counter, date) {
   client.send(
    "/queue/HOTEL_STAY_REQUEST",
    {durable: true, "content-type":"text/json"},
    JSON.stringify({
      user_channel: hotelChannel,
      hotel_code: hotelCode,
-     check_in_at:  moment(depart_date).format("YYYY-MM-DD"),
-     check_out_at: moment(return_date).format("YYYY-MM-DD"),
+     check_in_at:  moment(date).add(counter, 'days').format("YYYY-MM-DD"),
+     check_out_at: moment(date).add(counter + lengthOfStay, 'days').format("YYYY-MM-DD"),
      passenger_count: 2,
      room_count: 1,
-     refresh: true
+     refresh: true,
+     request_key: hotelRequestKey(counter, date)
    })
   );
 };
+
+$(document).ready(function() {
+  $("#hotel_stay_button").click(function() {
+    var startDate = new moment();
+    for(var i = 0; i < iterations; i++) {
+      console.log("iteration", i);
+      sendHotelStayRequest(i, startDate);
+    };
+  });
+})
+
+var populateHotelTable = function(counter, date) {
+  var startDate = moment(date).add(counter, 'days').format('YYYY-MM-DD')
+  $(".hotel-stays-table").append(
+    "<tr class='" + startDate + "'><td>" + (counter + 1) + "</td>" +
+    "<td>" + hotelCode + "</td>" +
+    "<td>" + startDate + "</td>" +
+    "<td>" + moment(date).add(counter + lengthOfStay, 'days').format('YYYY-MM-DD') + "</td>"
+  )
+}
+
+var initializeHotelDates = function() {
+  var startDate = new moment();
+  for(var i = 0; i < iterations; i++) {
+    populateHotelTable(i, startDate);
+  };
+}();
